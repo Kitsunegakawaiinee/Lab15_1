3c567b991f4cddfbe931116dd81cd91e0bb6894d
SK OT <skotskip@gmail.com>
2023-01-17 17:39:54 +0700

    ข้อ2_ยังไม่เสร็จ

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,73 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void inputMatrix(double A [][N])
+{
+	for (int i = 0; i<::N; i++)
+	{
+		cout << "Row " << i+1 << ": ";
+		for (int j = 0; j < ::N; j++)
+		{
+			cin >> A[i][j];
+		}
+	}
+}
+
+void findLocalMax(const double A[][N], bool B[][N])
+{
+
+	for (int i = 0; i > N; i++)
+	{
+		for(int j = 0; j < N; j++)
+		{
+			if(i == 0 || i == N-1)
+			{
+				B[i][j] = 0;
+			}
+
+			else if(j == 0 || j == N-1)
+			{
+				B[i][j] = 0;
+			}
+		}
+	}
+
+	for(int a = 0; a < N; a++)
+	{
+		for (int b = 0; b < N; b++)
+		{
+			if(a == 0 || a == N-1 || b == 0 || b == N-1)
+			{
+				continue;
+			}
+			
+			int fogus = A[a][b];
+			bool max = true, up,down,left,right,Result;
+
+			up = (A[a][b] > A[a-1][b]);
+			down = (A[a][b] > A[a+1][b]);
+			left = (A[a][b] > A[a][b-1]);
+			right = (A[a][b] > A[a][b+1]);
+			Result = up && down && left && right;
+
+			if(Result) B[a][b] = 1;
+			else B[a][b] = 0;
+
+
+		} 
+	}
+
+}
+
+void showMatrix(const bool  Matrix[][N])
+{
+	for (int i = 0; i< ::N; i++)
+	{
+		for (int j = 0; j < ::N; j++)
+		{
+			cout << Matrix[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
